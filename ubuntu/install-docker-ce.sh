2917e7f13469a201467930c499341471991a0f79
Stefan Scherer <scherer_stefan@icloud.com>
2017-06-07 22:53:40 +0200

    Add VERSION variable, edge is default

--- ubuntu/install-docker-ce.sh
+++ ubuntu/install-docker-ce.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+VERSION=${VERSION:-edge}
+
 sudo apt-get -y install \
   apt-transport-https \
   ca-certificates \
@@ -10,7 +12,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository \
        "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
        $(lsb_release -cs) \
-       edge"
+       $VERSION"
 
 sudo apt-get update
 
