baeddd95acfe2d4283bdc224b2550c383225f33e
Stefan Scherer <scherer_stefan@icloud.com>
2017-06-08 21:06:45 +0200

    Rename to CHANNEL

--- ubuntu/install-docker-ce.sh
+++ ubuntu/install-docker-ce.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=${VERSION:-edge}
+CHANNEL=${CHANNEL:-edge}
 
 sudo apt-get -y install \
   apt-transport-https \
@@ -14,7 +14,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository \
        "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
        $(lsb_release -cs) \
-       $VERSION"
+       $CHANNEL"
 
 sudo apt-get update
 
